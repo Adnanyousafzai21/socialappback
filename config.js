60d695e1cdf14dac8b87d2650089806383c54c68
Adnanyousafzai21 <www.adnanrafiq@gmail.com>
2023-11-20 13:05:24 +0500

    lalst commi

--- config.js
+++ config.js
@@ -1,6 +1,24 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/Blog")
+mongoose.connect("mongodb+srv://adnan12345:adnan123@blogapp.9sg5w0z.mongodb.net/")
 mongoose.connection.on("connected", () => {
     console.log("Connected to MongoDB");
   });
   
+
+//   const mongoose = require("mongoose");
+
+// // Replace the following connection string with your MongoDB Atlas connection string
+// const atlasConnectionUri = "mongodb+srv://adnan12345:adnan123@blogapp.9sg5w0z.mongodb.net/";
+
+// mongoose.connect(atlasConnectionUri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// mongoose.connection.on("connected", () => {
+//   console.log("Connected to MongoDB Atlas");
+// });
+
+// mongoose.connection.on("error", (err) => {
+//   console.error("MongoDB Atlas connection error:", err);
+// });
--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const cors = require("cors")
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.get("/", (req, res)=>{
+
+  res.send({message:"hello succesfully don"})
+})
 app.post("/register", async (req, res) => {
   try {
     const regest = new registration(req.body)
@@ -16,7 +20,7 @@ app.post("/register", async (req, res) => {
     if (error.code === 11000 && error.keyPattern.email) {
       res.status(400).send("email is already in use")
     } else {
-      console.log("Mongoose error:", error); // Log any Mongoose-specific errors
+
       res.status(500).send("An error occurred while saving the product.");
     }
 
@@ -26,11 +30,10 @@ app.post("/allblogs", async (req, res) => {
   try {
     const newblog = new allblogs(req.body);
     const saveblog = await newblog.save();
-    // console.log(saveblog)
+
     res.send(saveblog);
   }
   catch (erro) {
-    console.log("Mongoose error:", erro); // Log any Mongoose-specific errors
     res.status(500).send("An error occurred while saving the product.");
   }
 });
@@ -39,8 +42,7 @@ app.get("/allblogs", async (req, res) => {
     const getdata = await allblogs.find();
     res.send(getdata);
   } catch (error) {
-    console.log(error);
-    res.status(500).send("An error occurred while fetching the data.");
+    res.status(500).send("An error occurred while fetching the data.", error);
   }
 });
 
@@ -50,8 +52,7 @@ app.get("/blogs/:firstname", async (req, res) => {
     const getdata = await allblogs.find({ firstname: firstname });
     res.send(getdata);
   } catch (error) {
-    console.log(error);
-    res.status(500).send("An error occurred while fetching the data.");
+    res.status(500).send("An error occurred while fetching the data.", error);
   }
 });
 
@@ -65,27 +66,21 @@ app.delete("/blogs/:id", async (req, res) => {
       res.status(404).send("Blog not found");
     }
   } catch (error) {
-    console.log("error occour while deleting the blog", error)
-    res.status(500).send("an error accur while deleting the blog")
+    res.status(500).send("an error accur while deleting the blog", error)
   }
 })
 
 app.get("/getupdateblog/:id", async(req, res)=>{
  try{
   const { id } = req.params;
-  console.log("Received id:", id);
   const getdata = await allblogs.findOne({ _id: id });
-  console.log("Retrieved data:", getdata);
   if (getdata) {
     res.send(getdata);
-    console.log("Data found and sent");
   } else {
     res.status(404).send("Data not found");
-    console.log("Data not found");
   }
  }catch(error){
   res.status(500).send("an error while getting data". error)
-  console.log(error,"there is and error")
  }
 
 })
